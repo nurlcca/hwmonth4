abf919c3a615b9cea79d977192ba8b22a7944759
nurlcca <lh52174@truman.edu>
2026-04-17 21:45:30 +0600

    homework3

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,25 @@
 from django.db import models
+from django.conf import settings
+
+
+class Author(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
+    author = models.ForeignKey(
+    Author,
+    on_delete=models.CASCADE,
+    related_name='posts',
+    null=True,
+    blank=True
+)
     title = models.CharField(max_length=255)
     content = models.TextField()
     is_published = models.BooleanField(default=False)
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
 def post_list(request):
     posts = Post.objects.filter(is_published=True).order_by('-published_at')
     return render(request, 'posts/post_list.html', {'posts': posts})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, is_published=True)
+    return render(request, 'posts/post_detail.html', {'post': post})
