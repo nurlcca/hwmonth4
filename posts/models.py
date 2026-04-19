60753f4454097238fa0f3b0d74006737bfc85254
nurlcca <lh52174@truman.edu>
2026-04-19 10:17:40 +0600

    homework5

--- posts/models.py
+++ posts/models.py
@@ -12,18 +12,27 @@ class Author(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     author = models.ForeignKey(
-    Author,
-    on_delete=models.CASCADE,
-    related_name='posts',
-    null=True,
-    blank=True
-)
+        Author,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        null=True,
+        blank=True
+    )
     title = models.CharField(max_length=255)
     content = models.TextField()
     is_published = models.BooleanField(default=False)
     published_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
 
     def __str__(self):
         return self.title
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Tag
 
 
 def post_list(request):
-    posts = Post.objects.filter(is_published=True).order_by('-published_at')
-    return render(request, 'posts/post_list.html', {'posts': posts})
+    tag_id = request.GET.get('tag')
+    posts = Post.objects.filter(is_published=True).order_by('-created_at')
+    tags = Tag.objects.all()
+
+    if tag_id:
+        posts = posts.filter(tags__id=tag_id)
+
+    return render(request, 'posts/post_list.html', {
+        'posts': posts,
+        'tags': tags,
+        'selected_tag': tag_id,
+    })
 
 
 def post_detail(request, id):
