2c369c9cfefd96d708abd9bfb515e931dc71785a
nurlcca <lh52174@truman.edu>
2026-04-19 10:28:47 +0600

    homework5

--- config/settings.py
+++ config/settings.py
@@ -51,11 +51,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
